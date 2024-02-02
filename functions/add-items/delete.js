8c83ec7004f2a2b81145b20adb731f9b10334785
Asindu <hi@asindu.xyz>
2024-02-02 21:19:24 +0300

    feat: Added unique IDs

--- functions/add-items/delete.js
+++ functions/add-items/delete.js
@@ -1,4 +1,5 @@
 export function onRequest() {
+  // Return empty page to signal list deletion
     return new Response('', {
       headers: { 'Content-Type': 'text/html' },
     })
--- functions/add-items/populate-list.js
+++ functions/add-items/populate-list.js
@@ -1,13 +1,32 @@
 export async function onRequest(context) {
 
+  // Generate unique IDs for each list element
+  var csrng_api = 'https://csprng.xyz/v1/api';
+
+  var rn = await fetch(csrng_api, {})
+    .then(response => response.json())
+    .then(data => data['Data'])
+    .catch(error => error);
+
+  const encoded_rn = new TextEncoder().encode(`${rn}`);
+
+  const encoded_rn_digest = await crypto.subtle.digest(
+    {
+      name: 'MD5',
+    },
+    encoded_rn
+  );
+  var list_rn = 'ID'.concat(`${new Uint8Array(encoded_rn_digest)[0]}`, `${new Uint8Array(encoded_rn_digest)[1]}`, `${new Uint8Array(encoded_rn_digest)[2]}`, `${new Uint8Array(encoded_rn_digest)[3]}`);
+
+  //Get list item from form
   var url_string = context.request.url
   var url = new URL(url_string);
-
   var list_item = url.searchParams.get('list_item');
 
+  // Render list item with unique ID
   var list_item_html = `
-  <span id="abx">
-  <li><p><span><small class="finished-btn" hx-indicator="#remove-indicator" hx-delete="/add-items/delete" hx-target="#abx" hx-swap="outerHTML">👍 Finish.</small></p></span> ${list_item}. </li>
+  <span id="${list_rn}">
+  <li><p><span><small class="finished-btn" hx-indicator="#remove-indicator" hx-delete="/add-items/delete" hx-target="#${list_rn}" hx-swap="outerHTML">👍 Finish.</small></p></span> ${list_item}. </li>
   <hr/>
   </span>
     `;
